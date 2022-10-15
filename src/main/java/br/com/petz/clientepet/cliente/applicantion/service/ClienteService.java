d6d0d5aa313e2915dbdff735921f51bb152112ae
welingtonalcantara <welingtonalcantara@gmail.com>
2022-10-15 17:03:18 -0300

    implementa controller e fluxo par service

---
+++ src/main/java/br/com/petz/clientepet/cliente/applicantion/service/ClienteService.java
@@ -0,0 +1,8 @@
+package br.com.petz.clientepet.cliente.applicantion.service;
+
+import br.com.petz.clientepet.cliente.applicantion.api.ClienteRequest;
+import br.com.petz.clientepet.cliente.applicantion.api.ClienteResponse;
+
+public interface ClienteService {
+	ClienteResponse criacliente(ClienteRequest clienteRequest);
+}
