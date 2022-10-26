7b59e4b4ee7ea0ac766e3baef6ce64ca4dd2526a
welingtonalcantara <welingtonalcantara@gmail.com>
2022-10-26 20:32:24 -0300

    implementa petService buscaPet detalhado

--- src/main/java/br/com/petz/clientepet/pet/aplication/service/PetApplicationService.java
+++ src/main/java/br/com/petz/clientepet/pet/aplication/service/PetApplicationService.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Service;
 
 import br.com.petz.clientepet.cliente.applicantion.service.ClienteService;
+import br.com.petz.clientepet.pet.aplication.api.PetClienteDetalheResponse;
 import br.com.petz.clientepet.pet.aplication.api.PetClienteListResponse;
 import br.com.petz.clientepet.pet.aplication.api.PetRequest;
 import br.com.petz.clientepet.pet.aplication.api.PetResponse;
@@ -40,6 +41,13 @@ public class PetApplicationService implements PetService {
 		List<Pet> petsDoCliente = petRepository.buscaPetsDoClienteComID(idCliente);
 		log.info("[finish] PetApplicationService - buscaPetsDoClienteComID");
 		return PetClienteListResponse.converte(petsDoCliente);
+	}
+
+	@Override
+	public PetClienteDetalheResponse buscaPetDoClienteComID(UUID idCliente, UUID idPet) {
+		log.info("[start] PetApplicationService - buscaPetDoClienteComID");
+		log.info("[finish] PetApplicationService - buscaPetDoClienteComID");
+		return null;
 	}
 
 }
--- src/main/java/br/com/petz/clientepet/pet/aplication/service/PetService.java
+++ src/main/java/br/com/petz/clientepet/pet/aplication/service/PetService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
+import br.com.petz.clientepet.pet.aplication.api.PetClienteDetalheResponse;
 import br.com.petz.clientepet.pet.aplication.api.PetClienteListResponse;
 import br.com.petz.clientepet.pet.aplication.api.PetRequest;
 import br.com.petz.clientepet.pet.aplication.api.PetResponse;
@@ -13,5 +14,6 @@ public interface PetService {
 
 	PetResponse criaPet(UUID idCliente, @Valid PetRequest petRequest);
 	List<PetClienteListResponse> buscaPetsDoClienteComID(UUID idCliente);
+	PetClienteDetalheResponse buscaPetDoClienteComID(UUID idCliente, UUID idPet);
 
 }
