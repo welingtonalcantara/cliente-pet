0594da264bc4711d8ec50d0259ff9312f73cbf02
welingtonalcantara <welingtonalcantara@gmail.com>
2023-08-28 21:42:00 -0300

    implementa handler

--- src/main/java/br/com/petz/clientepet/cliente/infra/clienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/clienteInfraRepository.java
@@ -3,10 +3,12 @@ package br.com.petz.clientepet.cliente.infra;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import br.com.petz.clientepet.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -38,7 +40,7 @@ public class clienteInfraRepository implements ClienteRepository {
 		//----clienteSpringDataJPARepository retorna um optional. eu quero que vc pegue o que tá dentro do optional
 		// (que deve ser um cliente) e caso não tiver lance uma execeptional
 		Cliente cliente = clienteSpringDataJPARepository.findById(idCliente)
-				.orElseThrow(() -> new RuntimeException("cliente não encontrado!"));
+				.orElseThrow(() -> APIException.build(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
 		log.info("[finaliza] buscaClienteAtravesId");
 		return cliente;
 	}
