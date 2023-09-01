32b71ba09830af943166c18ed510fc185a31d086
welingtonalcantara <welingtonalcantara@gmail.com>
2023-09-01 20:38:18 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetRequest.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetRequest.java
@@ -1,5 +1,33 @@
 package br.com.petz.clientepet.pet.application.api;
 
-public class PetRequest {
+import java.time.LocalDate;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.com.petz.clientepet.pet.domain.Porte;
+import br.com.petz.clientepet.pet.domain.SexoPet;
+import br.com.petz.clientepet.pet.domain.TipoPet;
+import lombok.Value;
+
+@Value
+public class PetRequest {
+	@NotBlank
+	private String nomePet;
+	private Porte porte;
+	@NotNull	
+	private TipoPet tipo;
+	private String microchip;
+	@NotBlank
+	private String raca;
+	@Enumerated(EnumType.STRING)
+	@NotNull
+	private SexoPet sexo;
+	private String pelagemCor;
+	@NotNull
+	private LocalDate dataNascimento;
+	private String rga;
+	private Integer peso;
 }
