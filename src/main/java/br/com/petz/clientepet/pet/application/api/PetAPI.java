c0b750e9d7cb102716cbfcaa752abb7b5c7db900
welingtonalcantara <welingtonalcantara@gmail.com>
2023-09-03 10:51:37 -0300

    implementa conversao listPet em PetclienteListResponse

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.petz.clientepet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.petz.clientepet.cliente.application.api.ClienteListResponse;
 
 @RestController
@@ -27,4 +28,6 @@ public interface PetAPI {
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.OK)
 	List<PetClienteListResponse> getPetsDoClienteComId(@PathVariable UUID idCliente);
+	
+
 }
